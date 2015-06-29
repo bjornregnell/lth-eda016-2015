211551c346c9277c62066cacbb27c88a10e6857c
bjornregnell <bjorn.regnell@cs.lth.se>
2015-06-29 16:49:45 +0200

    update build script to be dir aware

--- weekplan/build.sh
+++ weekplan/build.sh
@@ -1,2 +1,11 @@
-pandoc -r markdown_github weekplan.md -o weekplan.html
-pandoc -r markdown_github weekplan.md -o weekplan.tex
+#!/bin/bash
+
+# get my DIR: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# generate stuff from markdown using pandoc: http://pandoc.org/
+echo building $DIR/weekplan.html
+pandoc -r markdown_github $DIR/weekplan.md -o $DIR/weekplan.html
+echo building $DIR/weekplan.tex
+pandoc -r markdown_github $DIR/weekplan.md -o $DIR/weekplan.tex
+
