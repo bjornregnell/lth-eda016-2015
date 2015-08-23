f6ebee26351ac9637acfd65a441f879c725a402e
bjornregnell <bjorn.regnell@cs.lth.se>
2015-08-23 22:19:43 +0200

    add variable exercise swap algorithm

---
+++ lectures/examples/terminal/swap/SwapQuest.java
@@ -0,0 +1,11 @@
+public class SwapQuest {
+    public static void main(String[] args){
+        int x = 42;
+        int y = 21;
+        System.out.println("x: " + x + "  y: " + y);
+
+        // ??? skriv satser som byter värden mellan x och y
+        
+        System.out.println("x: " + x + "  y: " + y);
+    }
+}
