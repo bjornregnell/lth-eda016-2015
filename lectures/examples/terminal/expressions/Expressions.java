31e66be82c3842301ba1be27a7852e13718453e2
bjornregnell <bjorn.regnell@cs.lth.se>
2015-08-24 19:37:15 +0200

    add slides on java expressions

---
+++ lectures/examples/terminal/expressions/Expressions.java
@@ -0,0 +1,15 @@
+public class Expressions {
+    public static void main(String[] args){
+
+        System.out.println( 42    )  // Heltalsvärde
+        System.out.println( 42.0  )  // Decimaltalsvärde
+        System.out.println( "Hej" )  // Strängvärde
+        System.out.println( true )  // Booelskt värde
+
+        System.out.println( 41 + 1 )         // Heltalsuttryck
+        System.out.println( 41.0 + 1.0 )     // Decimaltalsuttryck
+        System.out.println( "Hej" + "san!" ) // Stränguttryck
+        System.out.println( true && false )  // Booelskt uttryck
+        
+    }
+}
