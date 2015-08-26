3f12229081a60fa8623997d544787df48f09b8a8
bjornregnell <bjorn.regnell@cs.lth.se>
2015-08-26 18:07:13 +0200

    add 'ordinarie' to coursplan

--- courseplan/build.sh
+++ courseplan/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 # generate tex from markdown using pandoc: http://pandoc.org/
+echo ""
+echo "*** generating pandoc ***"
+echo ""
+pandoc --version
+echo ""
+echo pandoc --read markdown_github body.md -o generated-body.tex
+echo ""
 pandoc --read markdown_github body.md -o generated-body.tex
+
+echo ""
+echo "*** generating pdf ***"
+echo ""
+echo pdflatex courseplan.tex
+echo ""
+
 pdflatex courseplan.tex
