fb6cb78594517254adeb8f5db2435c74804916bf
bjornregnell <bjorn.regnell@cs.lth.se>
2015-08-26 22:11:34 +0200

    update slides on algoritm swap

--- lectures/examples/terminal/swap/SwapQuest.java
+++ lectures/examples/terminal/swap/SwapQuest.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;  
+
 public class SwapQuest {
     public static void main(String[] args){
-        int x = 42;
-        int y = 21;
+    	//Steg 1: skapa en Scanner
+		Scanner scan = new Scanner(System.in); 
+
+        int x = scan.nextInt();  //Steg 2: läs in x
+        int y = scan.nextInt();  //Steg 3: läs in y
+
+        //Steg 4: Skriv ut x och y
         System.out.println("x: " + x + "  y: " + y);
 
-        // ??? skriv satser som byter värden mellan x och y
-        
+        //Steg 5: byt plats på värdena mellan x och y  HUR???
+        // ... skriv SWAP-satser här ...
+        //Steg 6: Skriv ut x och y
         System.out.println("x: " + x + "  y: " + y);
     }
 }
