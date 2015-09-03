94e36ae6057ca22bb77c653c5f781df62ba2a665
bjornregnell <bjorn.regnell@cs.lth.se>
2015-09-03 22:13:11 +0200

    add slides and examples for week02 refvars

---
+++ lectures/examples/terminal/refvars/ReferenceVariables.java
@@ -0,0 +1,20 @@
+class Gurka {
+    public int vikt = 100; //gram 
+}
+
+public class ReferenceVariables {
+    public static void main(String[] args){
+        Gurka g1 = new Gurka();
+        Gurka g2 = new Gurka();
+        g2.vikt = 200;
+        System.out.println("Gurka 1 väger: " + g1.vikt + "g");
+        System.out.println("Gurka 2 väger: " + g2.vikt + "g");
+        g1.vikt = 200;
+        System.out.println("Gurka 1 väger nu: " + g1.vikt + "g");
+        if (g1 == g2) {
+          System.out.println("samma");
+        } else {
+          System.out.println("olika");
+        }
+    }
+}
