46f99e9fbf141b68f3e7715d2c30e232fd201209
bjornregnell <bjorn.regnell@cs.lth.se>
2015-09-22 12:56:10 +0200

    fix param values

--- lectures/examples/eclipse-ws/lecture-examples/src/week04/BankTest.java
+++ lectures/examples/eclipse-ws/lecture-examples/src/week04/BankTest.java
@@ -3,12 +3,12 @@ package week04;
 public class BankTest {
 
 	public static void main(String[] args) {
-		BankAccountWithInterest ba = new BankAccountWithInterest(42, 0.04);
+		BankAccountWithInterest ba = new BankAccountWithInterest(42, 4);
 		Date.setToday(100);
 		ba.deposit(50000);
 		Date.setToday(110);
 		ba.deposit(50000);
-		Date.setToday(365);
+		Date.setToday(360);
 		ba.newYearActions();
 		System.out.println("Balance: " + ba.getBalance());
 	}
