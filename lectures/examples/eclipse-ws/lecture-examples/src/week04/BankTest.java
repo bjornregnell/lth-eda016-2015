613c2edb542834ebaed207cf71fa3bfd351980bb
bjornregnell <bjorn.regnell@cs.lth.se>
2015-09-22 09:53:15 +0200

    update BankTest

--- lectures/examples/eclipse-ws/lecture-examples/src/week04/BankTest.java
+++ lectures/examples/eclipse-ws/lecture-examples/src/week04/BankTest.java
@@ -4,7 +4,13 @@ public class BankTest {
 
 	public static void main(String[] args) {
 		BankAccountWithInterest ba = new BankAccountWithInterest(42, 0.04);
-		ba.deposit(42);
+		Date.setToday(100);
+		ba.deposit(50000);
+		Date.setToday(110);
+		ba.deposit(50000);
+		Date.setToday(365);
+		ba.newYearActions();
+		System.out.println("Balance: " + ba.getBalance());
 	}
 
 }
