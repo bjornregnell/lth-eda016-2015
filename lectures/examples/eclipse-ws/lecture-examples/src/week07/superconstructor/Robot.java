c697f18fe2a60c32393f0d2a392f0c2dd4d50c0b
bjornregnell <bjorn.regnell@cs.lth.se>
2015-10-10 00:44:54 +0200

    update slides week07 superconstructor

--- lectures/examples/eclipse-ws/lecture-examples/src/week07/superconstructor/Robot.java
+++ lectures/examples/eclipse-ws/lecture-examples/src/week07/superconstructor/Robot.java
@@ -1,7 +1,7 @@
 package week07.superconstructor;
 
 public class Robot {
-	protected String name;
+	protected String name;  // protected: visible in subclasses
 
 	public Robot(String name) {
 		this.name = name;
--- lectures/examples/eclipse-ws/lecture-examples/src/week07/superconstructor/TalkingRobot.java
+++ lectures/examples/eclipse-ws/lecture-examples/src/week07/superconstructor/TalkingRobot.java
@@ -2,10 +2,10 @@ package week07.superconstructor;
 
 public class TalkingRobot extends Robot {
 	public TalkingRobot(String name) {
-		super(name);
+		super(name);  // call superclass constructor
 	}
 
 	public void talk() {
-		System.out.println(name + " shall not harm humans.");
+		System.out.println(name + " shall not harm humans."); // name visible in subclass
 	}
 }
