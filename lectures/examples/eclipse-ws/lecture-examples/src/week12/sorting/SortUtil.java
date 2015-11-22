487ced57a8361c695e406251ec010f94079270ca
bjornregnell <bjorn.regnell@cs.lth.se>
2015-11-22 21:49:05 +0100

    add more slides and examples and code week12

--- lectures/examples/eclipse-ws/lecture-examples/src/week12/sorting/SortUtil.java
+++ lectures/examples/eclipse-ws/lecture-examples/src/week12/sorting/SortUtil.java
@@ -33,13 +33,13 @@ public class SortUtil {
         }
         return xs;
     }
-    
-    public static ArrayList<Integer> insertionSortCopy(ArrayList<Integer> xs){
+
+    public static ArrayList<Integer> insertionSortCopy(ArrayList<Integer> xs) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        for (int x: xs){
+        for (int x : xs) {
             // find the right poosition for x in result
             int pos = 0;
-            while (pos < result.size() && result.get(pos) < x){
+            while (pos < result.size() && result.get(pos) < x) {
                 pos++;
             }
             // insert x in result
@@ -48,12 +48,12 @@ public class SortUtil {
         return result;
     }
 
-    public static ArrayList<Integer> selectionSortMove(ArrayList<Integer> xs){
+    public static ArrayList<Integer> selectionSortMove(ArrayList<Integer> xs) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        while (xs.size() > 0){
+        while (xs.size() > 0) {
             // find index of minimum value in xs
             int indexOfMin = 0;
-            for (int i = 1; i < xs.size(); i++){
+            for (int i = 1; i < xs.size(); i++) {
                 if (xs.get(i) < xs.get(indexOfMin)) {
                     indexOfMin = i;
                 }
@@ -63,38 +63,49 @@ public class SortUtil {
             result.add(x);
         }
         return result;
-    }    
-    
-    public static void swap(int[] xs, int a, int b){
+    }
+
+    public static void swap(int[] xs, int a, int b) {
         int temp = xs[a];
         xs[a] = xs[b];
-        xs[b] = temp; 
+        xs[b] = temp;
     }
-    
-    public static void insertionSortInPlaceSwap(int[] xs){
+
+    public static void insertionSortInPlaceSwap(int[] xs) {
         for (int i = 1; i < xs.length; i++) {
             int j = i;
-            while (j > 0 && xs[j - 1] > xs[j]){
+            while (j > 0 && xs[j - 1] > xs[j]) {
                 swap(xs, j, j - 1);
                 j = j - 1;
             }
         }
-    }    
+    }
 
-    public static void insertionSortInPlace(int[] xs){
+    public static void insertionSortInPlace(int[] xs) {
         for (int i = 1; i < xs.length; i++) {
             int current = xs[i];
             int j = i;
-            while (j > 0 && xs[j - 1] > current){
+            while (j > 0 && xs[j - 1] > current) {
                 xs[j] = xs[j - 1];
                 j--;
             }
             xs[j] = current;
         }
-    }  
-    
-    public static void selectionSortInPlace(int[] xs){
+    }
 
-    }      
-   
+    public static void selectionSortInPlace(int[] xs) {
+        for (int i = 0; i < xs.length - 1; i++) {
+            int min = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for (int k = i; k < xs.length; k++) {
+                if (xs[k] < min) {
+                    min = xs[k];
+                    minIndex = k;
+                }
+            }
+            xs[minIndex] = xs[i]; // låt xs[i] och
+            xs[i] = min;          // xs[minIndex] byta plats
+        }
+    }
+
 }
